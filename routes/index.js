390e5737cab75181d1a872586b2189d747b22c01
Jaime Flores <opiatamus79@gmail.com>
2019-03-16 03:10:44 -0500

    Adding project details page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Express' });
+});
+
+
+/* GET about page. */
+router.get('/project-swarm', function(req, res, next) {
+  res.render('project-swarm', { title: 'Express' });
+});
+
+
+
 module.exports = router;
