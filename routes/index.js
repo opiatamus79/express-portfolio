54a7b36031a7b83a95e9d77cad444073abefc5bf
Jaime Flores <opiatamus79@gmail.com>
2019-03-16 14:02:18 -0500

    Setting up the project details view for Donna project, setup banner, and added route for project details.

--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
 });
 
 
 
 /* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'Express' });
+  res.render('about', { title: 'About' });
 });
 
 
 /* GET about page. */
 router.get('/project-swarm', function(req, res, next) {
-  res.render('project-swarm', { title: 'Express' });
+  res.render('project-swarm', { title: 'Project-Detail' });
 });
 
+
+/* GET about page. */
+router.get('/project-donna', function(req, res, next) {
+  res.render('project-donna', { title: 'Project-Detail' });
+});
 
 
 module.exports = router;
