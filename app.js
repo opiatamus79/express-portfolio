bbaff5c0c4a9fecb3cccd71b9ad81f45526c183d
Jaime Flores <opiatamus79@gmail.com>
2019-03-17 13:29:06 -0500

    Adding tob logo

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var commitsRouter = require('./routes/commits');
 
 var app = express();
 
@@ -27,7 +27,7 @@ app.use(sassMiddleware({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/commits', commitsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/commits.js
+++ routes/commits.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+
+
+/* GET test page. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('commit-history', { title: 'Commit History' });
 });
+
+
+
+
 
 module.exports = router;
